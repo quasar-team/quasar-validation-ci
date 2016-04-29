91561d159f73a4a98161d9d6e265ae51221cf77c
Damian Abalo Miron <wardamo.zero@gmail.com>
2016-04-29 13:25:31 +0200

    Test travic CI build added for evaluation version 1.5.1, working with the branch of QUASAR uatoolkit_14x. Added ServerConfig.xml also, since it is at the moment not included with QUASAR. The old open6 test travis script was moved to .travis.yml.open6 to avoid loosing info

--- CI/ubuntu_evaluationToolkit.cmake
+++ CI/ubuntu_evaluationToolkit.cmake
@@ -1,5 +1,5 @@
 #OPC UA Toolkit
-SET( OPCUA_TOOLKIT_PATH "/opt/OpcUaToolkit-1.3.3-evaluation/" )
+SET( OPCUA_TOOLKIT_PATH "/opt/OpcUaToolkit-1.5.1-evaluation/" )
 SET( OPCUA_TOOLKIT_LIBS_DEBUG "-luamoduled -lcoremoduled -luabased -luastackd -luapkid -lssl -lcrypto -lxmlparserd -lxml2  -lpthread" ) 
 SET( OPCUA_TOOLKIT_LIBS_RELEASE "-luamodule -lcoremodule -luabase -luastack -luapki -lssl -lcrypto -lxmlparser -lxml2  -lpthread")
 
@@ -12,10 +12,10 @@ SET( BOOST_LIBS "-lboost_program_options -lboost_system" )
 SET( XML_LIBS "-lxerces-c" ) 
 
 #Other options
-SET(CMAKE_EXECUTABLE_SUFFIX ".slc6_evaluationtoolkit.exe" )
+SET(CMAKE_EXECUTABLE_SUFFIX ".ubuntu_evaluationtoolkit.exe" )
 
 add_definitions(-DSUPPORT_XML_CONFIG -Wall -DWIN32_LEAN_AND_MEAN -Wno-literal-suffix)
-set(CMAKE_CXX_FLAGS_RELEASE "-m32 -static-libstdc++ -std=gnu++0x")
-set(CMAKE_CXX_FLAGS_DEBUG "-m32 -static-libstdc++ -std=gnu++0x")
+set(CMAKE_CXX_FLAGS_RELEASE "-m64 -static-libstdc++ -std=gnu++0x")
+set(CMAKE_CXX_FLAGS_DEBUG "-m64 -static-libstdc++ -std=gnu++0x")
 
  
